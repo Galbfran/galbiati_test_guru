78dfbd3c21d3799444c0460baa3b8cc0da4491e4
Franco Galbiati <francogalbiati984@gmail.com>
2025-01-24 17:45:31 -0300

    add message image

--- src/components/SuscribeSection.tsx
+++ src/components/SuscribeSection.tsx
@@ -12,7 +12,7 @@ export default function SuscribeSection() {
 
   return (
     <section className="container">
-      <Icon src="public/assets/message_icon.svg" alt="logo" width={58} height={58} />
+      <Icon src="public/assets/message_image.png" alt="logo" width={58} height={58} />
       <Title title="subscribe" />
       <Input
         type="email"
